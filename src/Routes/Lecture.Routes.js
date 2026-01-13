0dd6dd2317b1b6619942fd932a5ec2f0843ba029
saahil0708 <saahil.ind.dgp@gmail.com>
2026-01-14 01:35:23 +0530

    ...

--- src/Routes/Lecture.Routes.js
+++ src/Routes/Lecture.Routes.js
@@ -5,36 +5,37 @@ const { verifyTokenWithSession } = require('../Middlewares/AuthMiddleware');
 const router = express.Router();
 
 // Lecture creation and modification (require authentication)
-router.post("/create", 
+router.post("/create",
     verifyTokenWithSession,
     invalidateCacheOnUpdate(['all_lectures', 'today_lectures']),
     createLecture
 );
 
-router.put("/:id", 
+router.put("/:id",
     verifyTokenWithSession,
     invalidateCacheOnUpdate(['all_lectures', 'today_lectures', 'single_lecture']),
     updateLecture
 );
 
-router.delete("/:id", 
+router.delete("/:id",
     verifyTokenWithSession,
     invalidateCacheOnUpdate(['all_lectures', 'today_lectures', 'single_lecture']),
     deleteLecture
 );
 
 // Lecture fetching with caching
-router.get("/all", 
+router.get("/all",
     cache('all_lectures', 300), // Cache for 5 minutes
     getAllLectures
 );
 
-router.get("/today", 
+router.get("/today",
+    verifyTokenWithSession,
     cache('today_lectures', 180), // Cache for 3 minutes (more frequent updates)
     getTodayLectures
 );
 
-router.get("/:id", 
+router.get("/:id",
     cache('single_lecture', 600), // Cache for 10 minutes
     getLectureById
 );
